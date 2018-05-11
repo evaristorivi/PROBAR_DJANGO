bc84851a8e50ed84160a7d986145bfa4327037a5
evaristorivi <evaristorivieccio@gmail.com>
2018-05-11 13:51:44 +0200

    8. Personalizar Modelo en el Admin

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Registrado
 
-
-admin.site.register(Registrado)
+class AdminRegistrado(admin.ModelAdmin):
+	list_display = ["email","nombre", "timestamp"]
+	# list_display_links = ["nombre"]
+	list_filter = ["timestamp"]
+	list_editable = ["nombre"]
+	search_field = ["email","nombre"]
+	class Meta:
+		model = Registrado
+admin.site.register(Registrado, AdminRegistrado)

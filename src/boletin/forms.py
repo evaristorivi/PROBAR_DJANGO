889cb5be0dac8f4ca2d2ec5ef6053df34a76c1f2
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-05-12 11:38:07 +0200

    17. Validaciones Model Form

--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -7,6 +7,18 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["nombre", "email"]
 
+	def clean_email(self):
+		email = self.cleaned_data.get("email")
+		email_base, proveeder = email.split("@")
+		dominio, extension = proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError("Por favor utiliza en email con la extenxion .EDU")
+		return email
+
+def clean_nombre(self):
+	nombre = self.cleaned_data.get("nombre")
+	#validaciones
+	return nombre
 class RegForm(forms.Form):
 	nombre = forms.CharField(max_length=100)
 	email = forms.EmailField()

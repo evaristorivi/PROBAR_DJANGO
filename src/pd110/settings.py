3d6f301a60369b43a58335bcbb06648066f056a5
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-05-13 20:38:29 +0200

    25. Django Crispy forms

--- src/pd110/settings.py
+++ src/pd110/settings.py
@@ -40,12 +40,16 @@ EMAIL_USE_TLS = True
 # Application definition
 
 INSTALLED_APPS = [
+    #apps django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps terceros
+    'crispy_forms',
+    #mis apps
     'boletin',
 ]
 
@@ -60,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pd110.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {

24ab2f63e098f8dc31e3346d496cb95aa87d97ce
Evaristo R Rivieccio Vega <evaristorivieccio@gmail.com>
2018-05-14 02:00:58 +0200

    28. Enlaces con Nombres URL

--- src/pd110/urls.py
+++ src/pd110/urls.py
@@ -19,11 +19,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from boletin import views
+from .views import about
 #from boletin.views inicio
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^contact$', views.contact, name='conctact'),
-	url(r'^$', views.inicio, name='inicio')
+    url(r'^contact$', views.contact, name='contact'),
+	url(r'^$', views.inicio, name='inicio'),
+    url(r'^about$', about, name='about')
+
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
